86a7c313f32ee486b2b36ec47fc5522e0c429658
chenji <insomnia@139.com>
2018-11-29 20:46:11 +0800

    Add admin tool client

---
+++ tools/admin.go
@@ -0,0 +1,272 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	. "github.com/journeymidnight/yig-iam/api/datatype"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"github.com/dgrijalva/jwt-go"
+	"bytes"
+)
+
+var client = &http.Client{}
+
+type Config struct {
+	RequestUrl string
+	AdminKey   string
+}
+
+var config Config
+
+var endPoint *string
+var token *string
+
+func printHelp() {
+	fmt.Println("Usage: admin <commands> [options...] ")
+	fmt.Println("Commands: login|bucket|object|user|cachehit")
+	fmt.Println("Options:")
+	fmt.Println(" -e, --endpoint   Specify endpoint of yig-iam")
+	fmt.Println(" -u, --user      Specify user name to login")
+	fmt.Println(" -p, --password   Specify password to login")
+	fmt.Println(" -t, --token   Specify token to send request with")
+	fmt.Println(" -m, --email   Specify email")
+	fmt.Println(" -d, --displayname   Specify display name")
+}
+
+func isParaEmpty(p string) bool {
+	if p == "" {
+		fmt.Printf("Bad usage, Try admin")
+		return true
+	} else {
+		return false
+	}
+}
+
+func prettyPrint(body []byte) {
+	if body == nil || len(body) == 0{
+		return
+	}
+	var prettyJSON bytes.Buffer
+	error := json.Indent(&prettyJSON, body, "", "\t")
+	if error != nil {
+		fmt.Println("bad json format", error.Error())
+		return
+	}
+	fmt.Printf(string(prettyJSON.Bytes()))
+}
+func login(user , password string) {
+	if isParaEmpty(user) ||  isParaEmpty(password){
+		return
+	}
+
+	query := &QueryRequest{}
+	query.UserName = user
+	query.Password = password
+
+	blob, _ := json.Marshal(query)
+
+	url := *endPoint + "/api/v1/login"
+	request, err := http.NewRequest("POST", url, bytes.NewReader(blob))
+	if err != nil {
+		fmt.Println("create request failed", err)
+		return
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println("send request failed", err)
+		return
+	}
+	if response.StatusCode != 200 {
+		fmt.Println("login failed as status != 200", response.StatusCode)
+		return
+	}
+
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	prettyPrint(body)
+}
+
+func createAccount(user , password, email, displayName string) {
+	if isParaEmpty(user) ||  isParaEmpty(password){
+		return
+	}
+
+	query := &QueryRequest{}
+	query.UserName = user
+	query.Password = password
+	query.Email = email
+	query.DisplayName = displayName
+
+	blob, _ := json.Marshal(query)
+
+	url := *endPoint + "/api/v1/account/create"
+	request, _ := http.NewRequest("POST", url, bytes.NewReader(blob))
+	request.Header.Set("Token", *token)
+	response, _ := client.Do(request)
+	if response.StatusCode != 200 {
+		fmt.Println("getBucketInfo failed as status != 200", response.StatusCode)
+		return
+	}
+
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	prettyPrint(body)
+}
+
+func listAccount() {
+	url := *endPoint + "/api/v1/account/list"
+	request, _ := http.NewRequest("GET", url, nil)
+	request.Header.Set("Token", *token)
+	response, _ := client.Do(request)
+	if response.StatusCode != 200 {
+		fmt.Println("getBucketInfo failed as status != 200", response.StatusCode)
+		return
+	}
+
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	prettyPrint(body)
+}
+
+func createProject(projectName, description string) {
+	if isParaEmpty(projectName) {
+		return
+	}
+
+	query := &QueryRequest{}
+	query.ProjectName = projectName
+	query.Description = description
+
+	blob, _ := json.Marshal(query)
+
+	url := *endPoint + "/api/v1/project/create"
+	request, _ := http.NewRequest("POST", url, bytes.NewReader(blob))
+	request.Header.Set("Token", *token)
+	response, _ := client.Do(request)
+	if response.StatusCode != 200 {
+		fmt.Println("createProject failed as status != 200", response.StatusCode)
+		return
+	}
+
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	prettyPrint(body)
+}
+
+func listkeys() {
+	url := *endPoint + "/api/v1/keys/list"
+	request, _ := http.NewRequest("GET", url, nil)
+	request.Header.Set("Token", *token)
+	response, _ := client.Do(request)
+	if response.StatusCode != 200 {
+		fmt.Println("listkeys failed as status != 200", response.StatusCode)
+		return
+	}
+
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	prettyPrint(body)
+
+}
+
+func getObjectInfo(bucket string, object string) {
+	if isParaEmpty(bucket) || isParaEmpty(object) {
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"bucket": bucket,
+		"object": object,
+	})
+
+	tokenString, err := token.SignedString([]byte(config.AdminKey))
+
+	if err == nil {
+		//go use token
+		fmt.Printf("\nHS256 = %v\n", tokenString)
+	} else {
+		fmt.Println("internal error", err)
+		return
+	}
+
+	url := config.RequestUrl + "/admin/object"
+	request, _ := http.NewRequest("GET", url, nil)
+	request.Header.Set("Authorization", "Bearer "+tokenString)
+	response, _ := client.Do(request)
+	if response.StatusCode != 200 {
+		fmt.Println("getBucketInfo failed as status != 200", response.StatusCode)
+		return
+	}
+
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
+
+func getCacheHit() {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{})
+
+	tokenString, err := token.SignedString([]byte(config.AdminKey))
+
+	if err == nil {
+		//go use token
+		fmt.Printf("\nHS256 = %v\n", tokenString)
+	} else {
+		fmt.Println("internal error", err)
+		return
+	}
+
+	url := config.RequestUrl + "/admin/cachehit"
+	request, _ := http.NewRequest("GET", url, nil)
+	request.Header.Set("Authorization", "Bearer "+tokenString)
+	response, _ := client.Do(request)
+	if response.StatusCode != 200 {
+		fmt.Println("getBucketInfo failed as status != 200", response.StatusCode)
+		return
+	}
+
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(body))
+
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		printHelp()
+		return
+	}
+	mySet := flag.NewFlagSet("", flag.ExitOnError)
+	user := mySet.String("u", "", "user")
+	password := mySet.String("p", "", "password")
+	endPoint = mySet.String("e", "http://127.0.0.1:8888", "endpoint")
+	token = mySet.String("t", "", "token")
+	email := mySet.String("m", "", "email")
+	displayName := mySet.String("d", "", "display name")
+	projectName := mySet.String("pn", "", "project name")
+	description := mySet.String("pd", "", "project description")
+	mySet.Parse(os.Args[2:])
+	fmt.Println("command:", os.Args[1], "user:", *user, "password:", *password)
+	fmt.Println("endPoint:", *endPoint, "token:", *token)
+	switch os.Args[1] {
+	case "login":
+		login(*user, *password)
+	case "createaccount":
+		createAccount(*user, *password, *email, *displayName)
+	case "listaccounts":
+		listAccount()
+	case "createproject":
+		createProject(*projectName, *description)
+	case "listkeys":
+		listkeys()
+	//case "cachehit":
+	//	getCacheHit()
+	default:
+		printHelp()
+		return
+	}
+}
